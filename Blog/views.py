93860a1cd6289e368148573f4e7a77a5769b5393
Clpy <clipeiyao@outlook.com>
2019-11-11 21:52:52 +0800

    显示热门文章的阅读排行

--- Blog/views.py
+++ Blog/views.py
@@ -1,14 +1,35 @@
 # coding=utf-8
+import datetime
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from read_statistics.utils import get_seven_days_read_data
+from read_statistics.utils import get_seven_days_read_data,\
+    get_today_hot_data, get_yesterday_hot_data
 from articles.models import Article
+from django.db.models import Sum
+from django.utils import timezone
+
+def get_7_days_hot_articles():
+    today = timezone.now().date()
+    date = today - datetime.timedelta(days=7)
+    articles = Article.objects\
+        .filter(read_details__date__lt=today,read_details__date__gte=date)\
+        .values('id','title')\
+        .annotate(read_num_sum = Sum('read_details__read_num'))\
+        .order_by('-read_num_sum')
+
+    return articles[:7]
 
 def index(request):
     article_content_type = ContentType.objects.get_for_model(Article)  # 获取模型类
     dates, read_nums = get_seven_days_read_data(article_content_type)
+    today_hot_data = get_today_hot_data(article_content_type)
+    yesterday_hot_data = get_yesterday_hot_data(article_content_type)
+    hot_articles_for_7_days = get_7_days_hot_articles()
     context = {
         'read_nums': read_nums,
-        'dates': dates
+        'dates': dates,
+        'today_hot_data': today_hot_data,
+        'yesterday_hot_data': yesterday_hot_data,
+        'hot_articles_for_7_days': hot_articles_for_7_days,
     }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html', context)
--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericRelation
 from mdeditor.fields import MDTextField
-from read_statistics.models import ReadNumExpandMethod
+from read_statistics.models import ReadNumExpandMethod, ReadDetail
 
 # Create your models here.
 class Category(models.Model):
@@ -21,6 +22,7 @@ class Article(models.Model, ReadNumExpandMethod):
     content = MDTextField()
     category = models.ForeignKey(Category,verbose_name='分类', on_delete=models.CASCADE, related_name='article_relate')
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+    read_details = GenericRelation(ReadDetail)
     created_time = models.DateTimeField('创建时间', auto_now_add=True)
     update_time = models.DateTimeField('更新时间', auto_now=True)
 
